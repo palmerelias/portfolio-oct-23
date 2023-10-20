7f63a83725dc35bc57f48bc1785254afa7410fba
Elias Palmer <elias@toddsden.com>
2023-10-20 12:09:08 -0400

    added files

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import "./styles.css"
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h3 className='name'>Elias</h3>
+        <img />
+      </div>
+      <div>
+        <h1 className='ocupation'>Frontend</h1>
+        <h1 className='ocupation'>Developer</h1>
+        <h4 className='ocupation-message'>I like to craft solid and scalable frontend projects with great user experiences.</h4>
+      </div>
+    </>
+  )
+}
+
+export default App
