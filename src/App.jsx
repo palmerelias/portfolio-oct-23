0b100753aa7c2a3474111fe2d34e4fb40ec6073c
Elias Palmer <elias@toddsden.com>
2023-10-24 22:11:08 -0400

    formated branch for mobile screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
 import { useState } from 'react'
 import "./styles.css"
+import Profile from "./profile.jpg"
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function App() {
 
   return (
     <>
       <nav>
-            <h1 className='name'>Elias</h1>
+            <h1 className='name-logo'>Elias</h1>
             <div className="right-nav">
                 <DarkModeOutlinedIcon className='dark-mode' fontSize='large'/>
-                <a
-                  className="resume"
-                  href="#"
-                >
-                  Resume
-                </a>
+                <a className="resume" href="#" >Resume</a>
             </div>
-          </nav>
+      </nav>
       <div className='info'>
-        <h2 className='ocupation-message-orange'>Elias Palmer</h2>
-        <h3 className='ocupation'>Frontend</h3>
-        <h3 className='ocupation'>Developer</h3>
+        <h2 className='name'>Elias Palmer</h2>
+        <h3 className='ocupation'>Frontend Dev Student</h3>
         <p className='ocupation-message'>I like to craft solid and scalable frontend projects with great user experiences.</p>
-        
+        <img className="profile-img" src={Profile} />
+      </div>
+      <div className='links'>
+        <a href="https://github.com/palmerelias"><GitHubIcon/></a>
       </div>
     </>
   )
